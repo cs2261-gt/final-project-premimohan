cfaa048d4fa05ac94a78798f0327be45dd417a05
Premi Mohanavelu <pmohanavelu@gatech.edu>
2020-04-22 20:31:39 -0400

    updated the instruction screen (ish)

--- instructScreen.h
+++ instructScreen.h
@@ -5,11 +5,11 @@
 //
 //	instructScreen, 256x256@4, 
 //	+ palette 256 entries, not compressed
-//	+ 136 tiles (t|f|p reduced) not compressed
+//	+ 250 tiles (t|f|p reduced) not compressed
 //	+ regular map (in SBBs), not compressed, 32x32 
-//	Total size: 512 + 4352 + 2048 = 6912
+//	Total size: 512 + 8000 + 2048 = 10560
 //
-//	Time-stamp: 2020-04-04, 22:29:54
+//	Time-stamp: 2020-04-22, 20:28:08
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -18,8 +18,8 @@
 #ifndef GRIT_INSTRUCTSCREEN_H
 #define GRIT_INSTRUCTSCREEN_H
 
-#define instructScreenTilesLen 4352
-extern const unsigned short instructScreenTiles[2176];
+#define instructScreenTilesLen 8000
+extern const unsigned short instructScreenTiles[4000];
 
 #define instructScreenMapLen 2048
 extern const unsigned short instructScreenMap[1024];
